c8f4df38c16cc11589a6866d9914eb753a9344f8
Pavel Tarasov <p.tarasov@alef.im>
2024-11-27 22:19:29 +0300

    initial commit

---
+++ src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST || 'localhost',
+            port: parseInt(process.env.DB_PORT, 10) || 5432,
+            username: process.env.DB_USER || 'admin',
+            password: process.env.DB_PASS || 'admin',
+            database: process.env.DB_NAME || 'book_library',
+            autoLoadEntities: true,
+            synchronize: true,
+        })
+    ],
+})
+export class DatabaseModule {}
